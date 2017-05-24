d4940fa5ea25593e534aa8ff38afb2db6e96089e
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-05-23 23:14:05 -0600

    Updated structure to prep for login page.

--- app/src/main/java/com/bignerdranch/android/mvpbyexample/main/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/mvpbyexample/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bignerdranch.android.mvpbyexample;
+package com.bignerdranch.android.mvpbyexample.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bignerdranch.android.mvpbyexample.R;
 
 public class MainActivity extends AppCompatActivity {
 
